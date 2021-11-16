1ff38eaa35d412e6c6aa48543b9ea9f83f775e5f
jagadhis <jwaran78@gmail.com>
2021-11-16 16:47:02 +0530

    commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var temp = document.querySelector(".temp");
 var desc = document.querySelector(".desc");
 var clouds = document.querySelector(".clouds");
 var button = document.querySelector(".submit");
+var wind = document.querySelector(".wind");
 
 button.addEventListener("click", function () {
   fetch(
@@ -16,10 +17,12 @@ button.addEventListener("click", function () {
       var tempValue = data["main"]["temp"];
       var nameValue = data["name"];
       var descValue = data["weather"][0]["description"];
+      var windvalue = data["wind"]["speed"];
 
       main.innerHTML = nameValue;
       desc.innerHTML = "Desc - " + descValue;
       temp.innerHTML = "Temp - " + tempValue;
+      wind.innerHTML = "Wind - " + windvalue;
       input.value = "";
     })
 
